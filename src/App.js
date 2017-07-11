91847f2e1bee4f88baeb05210e9a4078606daebd
gnovruzov2018 <gnovruzov2018@ada.edu.az>
2017-07-11 14:49:42 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ChatRoom from './components/ChatRoom';
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+          <h2>Welcome to React</h2>
+          <ChatRoom />
+        </div>
+            );
+  }
+}
+
+export default App;
